fd90980c990d314f8d036ec5e1a46153bd21899a
Nathaniel Edonmhen <nathanieledonmhenose@gmail.com>
2025-07-24 15:20:23 +0100

    added time module for continoursly organixation of file in the directory(folder)

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 import shutil
 import argparse
+import time
 
 parser = argparse.ArgumentParser(description="Smart File Organiser")
 
 parser.add_argument(
     "-f", "--folder", help="Enter the name of folder you intend to organise"
 )
+
+
 args = parser.parse_args()
 
 folder = Path(args.folder).resolve()
@@ -21,9 +24,13 @@ if not folder.exists():
     exit()
 
 
-user_option = input("To organise this folder once (y), to continously organise (c): ").strip().lower()
+user_option = (
+    input("To organise this folder once (y), to continously organise (c): ")
+    .strip()
+    .lower()
+)
 
-if user_option == 'y':
+if user_option == "y":
     print("Organising the folder once...")
     for file in folder.iterdir():
         if file.is_dir():
@@ -44,13 +51,14 @@ if user_option == 'y':
             financial.mkdir(parents=True, exist_ok=True)
             shutil.move(str(file), str(financial / file.name))
 
-elif user_option == 'c':
-    print("Continuously organising the folder...")
+elif user_option == "c":
+    print("Continuously organising this folder...")
     try:
         while True:
             for file in folder.iterdir():
                 if file.is_dir():
                     continue
+
                 elif file.suffix.lower() in [".txt", ".pdf", ".docx", ".xlsx", ".pptx"]:
                     document.mkdir(parents=True, exist_ok=True)
                     shutil.move(file, (document / file.name))
@@ -66,8 +74,15 @@ elif user_option == 'c':
                 elif file.suffix.lower() in [".csv", ".xls", ".xlsx"]:
                     financial.mkdir(parents=True, exist_ok=True)
                     shutil.move(str(file), str(financial / file.name))
+
+                time.sleep(1)
+        
     except KeyboardInterrupt:
-        print("\nExiting continuous organisation mode.")
+        print("\nContinuous organisation stopped by user.")
+        exit()
+      
 else:
-    print("Invalid option. Please enter 'y' to organise  once or 'c' for continuous organisation.")
+    print(
+        "Invalid option. Please enter 'y' to organise  once or 'c' for continuous organisation."
+    )
     exit()
